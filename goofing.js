4f1afa71751e921b6a217e193e8a5e58347d0647
JasonAspire <jasonm.lehmann@aspiritech.org>
2019-09-12 13:06:32 -0500

    Refactored

--- goofing.js
+++ goofing.js
@@ -1,62 +1,67 @@
 ("use strict");
 var TestArrays = require("./arraytemplate");
+
+const Swap = (Array, a) => {
+  [Array[a], Array[a + 1]] = [Array[a + 1], Array[a]];
+};
+
+const Compare = (Array, FinalResult) => {
+  JSON.stringify(Array) === JSON.stringify(FinalResult)
+    ? console.log("works")
+    : console.log(
+        "\x1b[41m" + "issue " + Array + " != " + FinalResult + "\x1b[0m"
+      );
+  //32 best record of comparisons.
+  console.log("\n");
+};
 //Instead of Resetting to 0 I just put the counter back a bit.
 const BubbleSort = Array => {
   console.log("BubbleSort Backing up a schooch" + "\n" + "Starting " + Array);
-
-  const Swap = a => {
-    [Array[a], Array[a + 1]] = [Array[a + 1], Array[a]];
-  };
-  var counter = 0;
+  var comparisonCounter = 0;
   for (var i = 0; i < Array.length - 1; i++) {
-    counter = counter + 1;
-
+    comparisonCounter++;
     if (Array[i] > Array[i + 1]) {
-      Swap(i);
-      i = i - 2;
+      Swap(Array, i);
+      i === 0 ? i-- : (i = i - 2);
     }
   }
+
+  //To Block
   console.log("Ending " + Array);
-  console.log("Total Counts  " + counter);
+  console.log("Total Counts  " + comparisonCounter);
+  return Array;
 };
 //Replicating The example
-const BubbleSortPlay = Array => {
+const BubbleSortSimple = Array => {
   console.log("Sort resetting to 0, Starting " + Array);
 
-  const Swap = a => {
-    [Array[a], Array[a + 1]] = [Array[a + 1], Array[a]];
-  };
-  var counter = 0;
+  var comparisonCounter = 0;
   for (var i = 0; i < Array.length - 1; i++) {
-    counter = counter + 1;
-
+    comparisonCounter++;
     if (Array[i] > Array[i + 1]) {
-      Swap(i);
+      Swap(Array, i);
       i = -1;
     }
   }
   console.log("Ending " + Array);
-  console.log("Total Counts  " + counter);
+  console.log("Total Counts  " + comparisonCounter);
+  return Array;
 };
 
 const BubbleSortLastCallCheck = Array => {
   console.log("Last Call \nStarting " + Array);
 
-  const Swap = a => {
-    [Array[a], Array[a + 1]] = [Array[a + 1], Array[a]];
-  };
-
-  var counter = 0;
+  var comparisonCounter = 0;
   var passCounter = 0;
-  var clean;
+  let clean = true;
   var distance = Array.length;
   do {
     clean = true;
     for (var i = 0; i < distance - 1; i++) {
-      counter = counter + 1;
+      comparisonCounter++;
 
       if (Array[i] > Array[i + 1]) {
-        Swap(i);
+        Swap(Array, i);
         if (clean && i === distance - 2) {
         } else {
           clean = false;
@@ -66,46 +71,18 @@ const BubbleSortLastCallCheck = Array => {
     passCounter++;
   } while (!clean);
   console.log("Ending " + Array);
-  console.log("Total Counts  " + counter + "\n Total Passes " + passCounter);
-};
-
-const BubbleSortFull = Array => {
-  console.log("Clean Swap, Starting " + Array);
-
-  const Swap = a => {
-    [Array[a], Array[a + 1]] = [Array[a + 1], Array[a]];
-  };
-
-  var counter = 0;
-  var passCounter = 0;
-  var clean;
-  var distance = Array.length;
-  do {
-    clean = true;
-    for (var i = 0; i < distance - 1; i++) {
-      counter = counter + 1;
-
-      if (Array[i] > Array[i + 1]) {
-        Swap(i);
-        clean = false;
-      }
-    }
-    passCounter++;
-  } while (!clean);
-  console.log("Ending " + Array);
-  console.log("Total Counts  " + counter + "\nTotal Passes " + passCounter);
+  console.log(
+    "Total Counts  " + comparisonCounter + "\n Total Passes " + passCounter
+  );
+  return Array;
 };
 
 const BubbleSortDynamicFor = Array => {
   console.log("DynamicFor \nStarting " + Array);
 
-  const Swap = a => {
-    [Array[a], Array[a + 1]] = [Array[a + 1], Array[a]];
-  };
-
-  var counter = 0;
+  var comparisonCounter = 0;
   var passCounter = 0;
-  var clean;
+  let clean = true;
   //I think I'm over abstracting here
   const distance = Array.length - 1;
 
@@ -116,7 +93,7 @@ const BubbleSortDynamicFor = Array => {
     clean = true;
 
     for (var i = beginMark; i < endMark; i++) {
-      counter = counter + 1;
+      comparisonCounter++;
 
       if (Array[i] > Array[i + 1]) {
         //Update Dynamic range
@@ -128,46 +105,38 @@ const BubbleSortDynamicFor = Array => {
           }
         }
 
-        if (i + 2 > distance) {
-          newEndMark = distance;
-        } else {
-          newEndMark = i + 2;
-        }
+        newEndMark = i + 2;
 
         //return to normal
-        Swap(i);
-
-        //Todo : figure out inverse of this since it is the last call
-        if (clean && i === distance - 1) {
-        } else {
-          clean = false;
-        }
+        Swap(Array, i);
+        clean = false;
       }
     }
     passCounter++;
-    console.log(Array);
-    console.log(
+
+    /* console.log(
       "Begin ==> newBegin : " + beginMark + "=> " + newBeginMark + "\n"
     );
-    console.log("End ==> newEnd : " + endMark + "=> " + newEndMark + "\n");
+    console.log("End ==> newEnd : " + endMark + "=> " + newEndMark + "\n");*/
     beginMark = newBeginMark;
     endMark = newEndMark;
-  } while (!clean);
+  } while ((!clean, JSON.stringify()));
   console.log("Ending " + Array);
-  console.log("Total Counts  " + counter + "\n Total Passes " + passCounter);
+  console.log(
+    "Total Counts  " + comparisonCounter + "\n Total Passes " + passCounter
+  );
+  return Array;
 };
 
 if (process.argv[2]) {
   BubbleSort(process.argv[2]);
 } else {
-  console.log(TestArrays);
   for (element in TestArrays) {
-    //BubbleSort(TestArrays[element].slice(0));
+    var comparison = TestArrays[element].slice(0).sort();
+    Compare(comparison, BubbleSort(TestArrays[element].slice(0)));
     console.log("\n");
-    BubbleSortLastCallCheck(TestArrays[element].slice(0));
+    Compare(comparison, BubbleSortLastCallCheck(TestArrays[element].slice(0)));
     console.log("\n");
-    BubbleSortDynamicFor(TestArrays[element].slice(0));
-    //32 best record of comparisons.
-    console.log("\n");
+    Compare(comparison, BubbleSortDynamicFor(TestArrays[element].slice(0)));
   }
 }
